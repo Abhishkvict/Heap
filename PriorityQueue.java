9dd73f2d7edde040c10f8918dde3dd0fb6732462
Abhishkvict <97436973+Abhishkvict@users.noreply.github.com>
2025-12-25 20:33:15 +0530

    Implement basic PriorityQueue with enqueue and dequeue

---
+++ PriorityQueue.java
@@ -0,0 +1,12 @@
+public class PriorityQueue {
+    private final Heap heap = new Heap();
+    public void enqueue(int item){
+        heap.insert(item);
+    }
+    public int dequeue(){
+       return heap.remove();
+    }
+    public boolean isEmpty(){
+        return heap.isEmpty();
+    }
+}
